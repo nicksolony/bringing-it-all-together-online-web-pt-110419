8db13db178d21a137501fb29c2ea3126645d1d6f
Nick Solonyy <nicksolony@gmail.com>
2020-02-04 02:26:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,7 +32,7 @@ class Dog
             VALUES(?,?);
             SQL
       DB[:conn].execute(sql,self.name,self.breed) 
-      binding.pry
+      #binding.pry
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs;")[0][0] 
       self
   end
